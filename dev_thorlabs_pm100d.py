30ee1e127e00f150578ba3344ae5e5824a9a5b81
Jack Mango <jackmango@g.harvard.edu>
2025-05-02 09:45:30 -0400

    Add pm100d power meter support

---
+++ dev_thorlabs_pm100d.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  7 16:55:25 2018
+
+@author: Lothar Maisenbacher/MPQ
+"""
+
+import pyvisa
+import logging
+import numpy as np
+
+from defs import LoggerError
+
+logger = logging.getLogger()
+
+class Device:
+
+    def __init__(self, device):
+        """Init device."""
+        super().__init__() 
+        self.init_visa()
+
+    def read_power(self):
+        """ Read power from device. """
+        return self.visa_query('MEASure:POWer?')
+        
+    def read_energy(self):
+        """ Read energy from device. """
+        return self.visa_query('MEASure:ENERgy?')
+    
+    def get_values(self):
+        """Read channels."""
+        chans = self.device['Channels']
+        readings = {}
+        for channel_id, chan in chans.items():
+            if chan['Type'] == 'Power':
+                value = self.read_power(chan['Type'])
+                readings[channel_id] = value
+            elif chan['Type'] == 'Energy':
+                value = self.read_energy(chan['Type'])
+                readings[channel_id] = value
+            else:
+                raise LoggerError(
+                    f'Unknown channel type \'{chan["Type"]}\' for channel \'{channel_id}\''
+                    +f' of device \'{self.device["Device"]}\'')
+        return readings
+
